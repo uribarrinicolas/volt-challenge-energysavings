50ab40e174f059c92159332eed53ee686c15c2d8
Nicolás Uribarri <uribarrinicolas@outlook.com>
2024-06-21 05:02:09 -0300

    feat: removed debug logs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ class Program
             Console.WriteLine("Usage: EnergySavings <input_household_consumption.json> <input_solar_output.json> <output_report.json>");
             return;
         }
+        Console.WriteLine("Starting process");
 
         var consumptionFilePath = args[0];
         var solarOutputFilePath = args[1];
@@ -22,15 +23,9 @@ class Program
         var householdConsumption = await FileHelper.ReadJsonFileAsync<List<HouseholdConsumptionModel>>(consumptionFilePath);
         var solarOutput = await FileHelper.ReadJsonFileAsync<List<SolarOutputModel>>(solarOutputFilePath);
 
-        Console.WriteLine($"TS={householdConsumption[600].Timestamp};EC={householdConsumption[600].ElectricityConsumption}kw;");
-        Console.WriteLine($"TS={solarOutput[600].Timestamp};SO={solarOutput[600].SolarOutput}kw;");
-
         var report = EnergyReportService.GenerateEnergyReport(householdConsumption, solarOutput);
 
-        Console.WriteLine($"TS={report[600].Timestamp};ENEC={report[600].ExternalNetworkElectricityConsumption}kw;S=${report[600].Savings};");
-
         await FileHelper.WriteJsonFileAsync(reportFilePath, report);
-        Console.WriteLine("Finishing");
-
+        Console.WriteLine("Finished");
     }
 }
